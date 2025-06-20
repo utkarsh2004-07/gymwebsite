fd974c0717174d0c19199a0a9df82894faa6981c
utkarsh singh <121781121+utkarsh2004-07@users.noreply.github.com>
2025-06-20 15:36:18 +0530

    Update User.Controller.js

--- backend/controller/User.Controller.js
+++ backend/controller/User.Controller.js
@@ -95,9 +95,11 @@ export const Login = async (req, res) => {
             // });
             // console.log("Setting token cookie:", token);
             res.cookie("token", token, {
-                httpOnly: true,
-                secure: true
-            });
+    httpOnly: true,
+    secure: true,
+    sameSite: "None", 
+   
+});
 
 
             res.status(200).json({
