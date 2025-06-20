9b22f4fe1f802a1338ca16cfbd45e9a5473fec5e
utkarsh singh <121781121+utkarsh2004-07@users.noreply.github.com>
2025-06-20 15:38:20 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cors from "cors"
+const cookieParser = require('cookie-parser');
 import connectDB from "./Database/Database.js";
 import userRoutes from './routes/User.routes.js'
 import otp from "./routes/OTP.routes.js"
@@ -15,6 +16,8 @@ app.use(cors({
   origin: 'https://gymwebsite-123.onrender.com',
   credentials: true,
 }));
+app.use(cookieParser());
+
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
